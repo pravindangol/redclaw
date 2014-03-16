46847196d8d40771cb3b6993a9e952d2d507ff4a
pravin <pravindangol@gmail.com>
2014-03-16 16:37:26 +1000

    added.

---
+++ wp-content/plugins/booking-calendar-contact-form/README.txt
@@ -0,0 +1,123 @@
+=== Booking Calendar Contact Form ===
+Contributors: codepeople
+Donate link: http://wordpress.dwbooster.com/calendars/booking-calendar-contact-form
+Tags: contact form,booking form,reservation form,calendar,form,payment form,paypal form,booking calendar,reservation calendar,calendar form,booking
+Requires at least: 3.0.5
+Tested up to: 3.6
+Stable tag: trunk
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Booking Calendar Contact Form creates a booking form with a reservation calendar or a classic contact form, connected to a PayPal payment button.
+
+== Description ==
+
+Booking Calendar Form main features:
+
+	» Booking form connected to PayPal
+	» Optional availability verification
+	» Season management
+	» full-day bookings or partial-day bookings as used in hotels
+	» Built-in captcha anti-spam
+	» Configurable email texts
+	» Configurable validation messages
+	» Printable bookings list
+	» ... and more features (see below)
+
+With the **Booking Calendar Contact Form** you can create a **classic contact form** or a **booking form with a reservation calendar**, connected to a PayPal payment button. The reservation calendar lets the customer select the start (ex: check-in) and end (ex: checkout) dates.
+
+The **reservation calendar** is an optional item, so it can be disabled to create a **general purpose contact form**.
+
+There are two types of bookings available in the calendar configuration: full day bookings or partial day bookings. With full day bookings the whole day is blocked / reserved while in partial day bookings the start and end dates are partially blocked as used for example in **room/hotel bookings**.
+
+Features:
+
+* **Booking form** connected to PayPal: After clicking the reservation / book button on the booking form, the user is redirected to PayPal and after completing the payment three emails will be sent: the automatic PayPal notification, the booking confirmation email to the user sent from the website and the notification email to the website administrator containing the booking details. At this point the booking will be confirmed and will appear in the bookings list and calendar. 
+* **Reservation / booking calendar** with optional availability verification: The dates are blocked in the calendar only if the "bookings overlap" option is enabled (it's enabled by default).
+* **Season management:** Configuration accepts different prices for different dates (ex: low / medium / high season prices, special date's prices, etc...)
+* Supports **full-day bookings** or **partial day bookings** as used in hotels / room reservations (details mentioned above)
+* Allows to disable/hide the booking calendar to convert the booking form in a **general purpose contact form**
+
+Other features also present in this version:
+
+* Built-in captcha anti-spam protection on the booking form
+* Easy visual selection of the start and end dates
+* Configurable email texts
+* Configurable validation messages
+* Lets to assign a user to the calendar, this way a user with editor access will access his/her own booking calendar
+* List of bookings with print option
+* Several configurable fields, settings and behaviors (date format, min/max dates, block dates, etc...)
+
+What isn't included in the free version described here?
+
+* The Form Builder for customizing the form is present only in the pro version. The free version works with the classic predefined form included on it.
+* The connection to PayPal is part of the booking process in the free version, so it cannot be disabled on it. The pro version has additional code to work with or without the PayPal connection.
+* Coupons/discount codes and other minor features are present only in the pro version. 
+
+For information about the pro version check the plugin's page: http://wordpress.dwbooster.com/calendars/booking-calendar-contact-form
+
+
+== Installation ==
+
+To install **Booking Calendar Contact Form**, follow these steps:
+
+1.	Download and unzip the booking plugin
+2.	Upload the entire booking-calendar-contact-form/ directory to the /wp-content/plugins/ directory
+3.	Activate the Booking Calendar Contact Form plugin through the Plugins menu in WordPress
+4.	Configure the booking form settings at the administration menu >> Settings >> Booking Calendar Contact Form. 
+5.	To insert the booking form into some content or post use the icon that will appear when editing contents
+
+== Frequently Asked Questions ==
+
+= Q: What means each field in the settings area? =
+
+A: The product's page contains detailed information about each field and customization:
+
+http://wordpress.dwbooster.com/calendars/booking-calendar-contact-form
+
+= Q: Can I customize the booking calendar colors? =
+
+A: This and other questions are already answered in the product's FAQ at this page:
+
+http://wordpress.dwbooster.com/faq/booking-calendar-contact-form
+
+= Q: How to disable the accommodation availability verification? =
+
+A: In the booking calendar administration area, set the field "Accept overlapped reservations?" to "No", this way the accommodation availability verification will be disabled. Note that the calendar will disappear from the admin area when you select this setting since its purpose is to define the available days for booking.
+
+= Q: When is blocked the reservation and sent the email with the rental information? =
+
+A: After clicking the submit / book button the customer is redirected to a PayPal payment page to submit the payment to confirm it. After completed the payment the reservation is saved into the database and calendar, the dates become un-available it the booking availability verification is enabled and the emails are sent with the booking information and the information entered by the customer on the booking form. At that point the booking information will appear also in the printable bookings list.
+
+= Q: Got this error message at PayPal after clicking the book button: "We cannot process this transaction...". Solution?
+
+A: Into the Booking Calendar Contact Form settings >> PayPal payment form configuration >> PayPal email, be sure to put your own PayPal email address instead the email placeholder put there as default. 
+
+= Q: How to translate the plugin texts? =
+
+A: If you don't want to edit the MO/PO files then just edit the texts that are at the beginning of the file "dex_scheduler.inc.php" (the booking page). The booking form validation texts can be edited from the administration area.
+
+= Q: Can I restrict the number of days to book? =
+
+A: This feature is available in the pro version.
+
+
+== Screenshots ==
+
+1. Booking Calendar form / contact form.
+2. Inserting booking calendar into a page.
+3. Managing the booking calendar.
+4. Editing the calendar and rental settings.
+5. Booking Calendar settings.
+
+== Changelog ==
+
+= 1.0 =
+* First stable version released.
+* More configuration options added.
+
+== Upgrade Notice ==
+
+= 1.0.1 =
+* Interface modifications.
+* Compatible the latest WP and jQuery versions
---
+++ wp-content/plugins/booking-calendar-contact-form/dex_scheduler.inc.php
@@ -0,0 +1,189 @@
+<?php
+
+  // Start:: Language constants, translate below:
+  // -----------------------------------------------
+  
+  $l_calendar     = __("Calendar");
+  $l_select_dates = __("Select start and end dates");
+  $l_p_select     = __("Please select start and end dates");
+  $l_select_start = __("Select Start Date");
+  $l_select_end   = __("Select End Date");
+  $l_cancel_c     = __("Cancel Selection");
+  $l_sucess       = __("Successfully");
+  $l_cost         = __("Cost");  
+  $l_coupon       = __("Coupon code (optional)");
+  $l_service      = __("Service");
+  $l_sec_code     = __("Please enter the security code");
+  $l_sec_code_low = __("Security Code (lowercase letters)");
+  $l_continue     = __("Continue");
+  
+  // End:: Language constants.
+  // -----------------------------------------------  
+  
+?>
+<?php if ( !defined('DEX_AUTH_INCLUDE') ) { echo 'Direct access not allowed.'; exit; } ?>
+<link href="<?php echo plugins_url('css/stylepublic.css', __FILE__); ?>" type="text/css" rel="stylesheet" />
+<link href="<?php echo plugins_url('css/cupertino/jquery-ui-1.8.20.custom.css', __FILE__); ?>" type="text/css" rel="stylesheet" />
+<form class="cpp_form" name="dex_bccf_pform" id="dex_bccf_pform" action="<?php get_site_url(); ?>" method="post" onsubmit="return doValidate(this);"><input name="dex_bccf_post" type="hidden" id="1" />
+<?php if ($option_calendar_enabled != 'false') { ?>
+<link rel="stylesheet" type="text/css" href="<?php echo plugins_url('TDE_RCalendar/all-css.css', __FILE__); ?>" />
+<script>
+var pathCalendar = "<?php echo cp_bccf_get_site_url(); ?>";
+var pathCalendar_full = pathCalendar + "/wp-content/plugins/<?php echo basename(dirname(__FILE__));?>/TDE_RCalendar";
+var minDateConfigTDE = "<?php $value = dex_bccf_get_option('calendar_mindate', DEX_BCCF_DEFAULT_CALENDAR_MINDATE); if ($value != '') echo date("n/j/Y", strtotime($value)); ?>";  //month/day/year like this "10/5/2008" or "now" for current date
+var maxDateConfigTDE = "<?php $value = dex_bccf_get_option('calendar_maxdate', DEX_BCCF_DEFAULT_CALENDAR_MAXDATE); if ($value != '') echo date("n/j/Y",strtotime($value)); ?>";  //month/day/year like this "10/5/2008" or "now" for current date
+var dex_global_date_format = '<?php echo dex_bccf_get_option('calendar_dateformat', DEX_BCCF_DEFAULT_CALENDAR_DATEFORMAT); ?>';
+var dex_global_start_weekday = '<?php echo dex_bccf_get_option('calendar_weekday', DEX_BCCF_DEFAULT_CALENDAR_WEEKDAY); ?>';
+</script>
+<script type="text/javascript" src="<?php echo plugins_url('TDE_RCalendar/all-scripts.js', __FILE__); ?>"></script>
+<?php if (count($myrows) < 2) { ?>
+  <div style="display:none">
+<?php } else {?>
+  <div>
+<?php } ?>
+<?php
+  echo $l_calendar.":";
+?>
+<br />
+<select name="dex_item" id="dex_item" onchange="dex_updateItem()">
+<?php
+  foreach ($myrows as $item)
+  {
+      echo '<option value='.$item->id.'>'.$item->uname.'</option>';
+  }
+?>
+</select>
+<br /><br />
+</div>
+<?php
+  echo $l_select_dates.":";
+?>
+<?php
+  foreach ($myrows as $item)
+  {
+?>
+<div id="calarea_<?php echo $item->id; ?>" style="display:none">
+ <input name="selDay_start<?php echo $item->id; ?>" type="hidden" id="selDay_start<?php echo $item->id; ?>" /><input name="selMonth_start<?php echo $item->id; ?>" type="hidden" id="selMonth_start<?php echo $item->id; ?>" /><input name="selYear_start<?php echo $item->id; ?>" type="hidden" id="selYear_start<?php echo $item->id; ?>" /><input name="selDay_end<?php echo $item->id; ?>" type="hidden" id="selDay_end<?php echo $item->id; ?>" /><input name="selMonth_end<?php echo $item->id; ?>" type="hidden" id="selMonth_end<?php echo $item->id; ?>" /><input name="selYear_end<?php echo $item->id; ?>" type="hidden" id="selYear_end<?php echo $item->id; ?>" />
+ <div style="z-index:1000;">
+ <div id="containerRCalendar<?php echo $item->id; ?>"></div>
+ </div>
+ <div style="clear:both;"></div>
+</div>
+<?php
+  }
+?>
+<div id="bccf_display_price"> 
+Price:          
+</div>
+<?php } else { ?><input name="dex_item" id="dex_item" type="hidden" value="<?php echo $myrows[0]->id; ?>" /><?php } ?>            
+<div id="selddiv" style="font-weight: bold;margin-top:0px;padding-top:0px;padding-right:3px;padding-left:3px;"></div>
+<script type="text/javascript"><?php if ($option_calendar_enabled != 'false') { ?>
+ var dex_current_calendar_item;
+ function dex_updateItem()
+ {
+    document.getElementById("calarea_"+dex_current_calendar_item).style.display = "none";
+    var i = document.dex_bccf_pform.dex_item.options.selectedIndex;
+    var selecteditem = document.dex_bccf_pform.dex_item.options[i].value;
+    dex_do_init(selecteditem);
+ }
+ function dex_do_init(id)
+ {
+    dex_current_calendar_item = id;
+    document.getElementById("calarea_"+dex_current_calendar_item).style.display = "";
+    initCalendar(id,'<?php echo dex_bccf_get_option('calendar_language', DEX_BCCF_DEFAULT_CALENDAR_LANGUAGE); ?>',false,<?php echo dex_bccf_get_option('calendar_mode',DEX_BCCF_DEFAULT_CALENDAR_MODE); ?>,'<?php echo $l_select_start; ?>','<?php echo $l_select_end; ?>','<?php echo $l_cancel_c; ?>','<?php echo $l_sucess; ?>');
+    document.getElementById("selddiv").innerHTML = "";
+ }
+ dex_do_init(<?php echo $myrows[0]->id; ?>);
+ var bccf_d1 = "";
+ var bccf_d2 = "";
+ function updatedate()
+ {
+    var a = (document.getElementById("selDay_start"+dex_current_calendar_item ).value != '');
+    var b = (document.getElementById("selDay_end"+dex_current_calendar_item ).value != '');
+    var c = false;
+    if (a) if (b) c = true;
+    if (c)
+    {   
+        var d1 = document.getElementById("selYear_start"+dex_current_calendar_item ).value+"-"+document.getElementById("selMonth_start"+dex_current_calendar_item ).value+"-"+document.getElementById("selDay_start"+dex_current_calendar_item ).value;
+        var d2 = document.getElementById("selYear_end"+dex_current_calendar_item ).value+"-"+document.getElementById("selMonth_end"+dex_current_calendar_item ).value+"-"+document.getElementById("selDay_end"+dex_current_calendar_item ).value;        
+        if (bccf_d1 != d1 || bccf_d2 != d2)
+        {
+            bccf_d1 = d1;
+            bccf_d2 = d2;
+            $dexQuery = jQuery.noConflict();
+            $dexQuery.ajax({
+              type: "GET",
+              url: "<?php echo cp_bccf_get_site_url(); ?>/?dex_bccf=getcost"+String.fromCharCode(38)+"dex_item="+dex_current_calendar_item+""+String.fromCharCode(38)+"from="+d1+""+String.fromCharCode(38)+"to="+d2,
+            }).done(function( html ) {
+                $dexQuery("#bccf_display_price").append('<b><?php echo $l_cost; ?>:</b> <?php echo dex_bccf_get_option('currency', DEX_BCCF_DEFAULT_CURRENCY); ?> '+html);
+            });
+        }    
+    }
+    else         
+    {
+        bccf_d1 = "";
+        bccf_d2 = "";
+        document.getElementById("bccf_display_price").innerHTML = '';
+    }    
+ } 
+ setInterval('updatedate()',200);<?php } ?> 
+ function doValidate(form)
+ {
+    $dexQuery = jQuery.noConflict();
+    document.dex_bccf_pform.dex_bccf_ref_page.value = document.location;<?php if ($option_calendar_enabled != 'false') { ?>    
+    if (document.getElementById("selDay_start"+dex_current_calendar_item).value == '' || document.getElementById("selDay_end"+dex_current_calendar_item).value == '')
+    {
+        alert('<?php echo $l_p_select; ?>.');
+        return false;
+    }<?php } ?>     
+<?php if (dex_bccf_get_option('dexcv_enable_captcha', TDE_BCCFDEFAULT_dexcv_enable_captcha) != 'false') { ?> if ($dexQuery("#hdcaptcha_dex_bccf_post").val() == '')
+    {
+        alert('<?php echo dex_bccf_get_option('cv_text_enter_valid_captcha', DEX_BCCF_DEFAULT_dexcv_text_enter_valid_captcha); ?>');
+        return false;
+    }
+    var result = $dexQuery.ajax({
+        type: "GET",
+        url: "<?php echo cp_bccf_get_site_url(); ?>?hdcaptcha_dex_bccf_post="+$dexQuery("#hdcaptcha_dex_bccf_post").val(),
+        async: false
+    }).responseText;
+    if (result == "captchafailed")
+    {
+        $dexQuery("#dex_bccf_captchaimg").attr('src', $dexQuery("#dex_bccf_captchaimg").attr('src')+'&'+Date());
+        alert('<?php echo dex_bccf_get_option('cv_text_enter_valid_captcha', DEX_BCCF_DEFAULT_dexcv_text_enter_valid_captcha); ?>');
+        return false;
+    }
+    else <?php } ?>
+        return true;
+ }
+</script><input type="hidden" name="dex_bccf_pform_process" value="1" /><input type="hidden" name="dex_bccf_id" value="<?php echo CP_BCCF_CALENDAR_ID; ?>" /><input type="hidden" name="dex_bccf_ref_page" value="<?php esc_attr(cp_bccf_get_FULL_site_url); ?>" /><input type="hidden" name="form_structure" id="form_structure" size="180" value="<?php echo str_replace("\r","",str_replace("\n","",esc_attr(dex_bccf_cleanJSON(dex_bccf_get_option('form_structure', DEX_BCCF_DEFAULT_form_structure))))); ?>" />
+  <div id="fbuilder">
+      <div id="formheader"></div>
+      <div id="fieldlist"></div>
+  </div>
+<div id="cpcaptchalayer">  
+<?php
+     $codes = $wpdb->get_results( 'SELECT * FROM '.DEX_BCCF_DISCOUNT_CODES_TABLE_NAME.' WHERE `cal_id`='.CP_BCCF_CALENDAR_ID);
+     if (count($codes))
+     {
+?>
+      <div class="fields" id="field-c0"> 
+         <label><?php echo $l_coupon; ?>:</label>
+         <div class="dfield"><input type="text" name="couponcode" value=""></div>
+         <div class="clearer"></div>
+      </div>
+<?php } ?>
+<?php
+ if ($dex_buffer != '')
+ {
+    echo '<div class="fields" id="field-c1"><label>';
+    echo $l_service;
+    echo ':</label><div class="dfield"><select name="services">'.$dex_buffer.'</select></div><div class="clearer"></div></div><br />';
+ }
+?>
+<?php if (dex_bccf_get_option('dexcv_enable_captcha', TDE_BCCFDEFAULT_dexcv_enable_captcha) != 'false') { ?>
+  <?php echo $l_sec_code; ?>:<br /><img src="<?php echo plugins_url('/captcha/captcha.php?width='.dex_bccf_get_option('dexcv_width', TDE_BCCFDEFAULT_dexcv_width).'&height='.dex_bccf_get_option('dexcv_height', TDE_BCCFDEFAULT_dexcv_height).'&letter_count='.dex_bccf_get_option('dexcv_chars', TDE_BCCFDEFAULT_dexcv_chars).'&min_size='.dex_bccf_get_option('dexcv_min_font_size', TDE_BCCFDEFAULT_dexcv_min_font_size).'&max_size='.dex_bccf_get_option('dexcv_max_font_size', TDE_BCCFDEFAULT_dexcv_max_font_size).'&noise='.dex_bccf_get_option('dexcv_noise', TDE_BCCFDEFAULT_dexcv_noise).'&noiselength='.dex_bccf_get_option('dexcv_noise_length', TDE_BCCFDEFAULT_dexcv_noise_length).'&bcolor='.dex_bccf_get_option('dexcv_background', TDE_BCCFDEFAULT_dexcv_background).'&border='.dex_bccf_get_option('dexcv_border', TDE_BCCFDEFAULT_dexcv_border).'&font='.dex_bccf_get_option('dexcv_font', TDE_BCCFDEFAULT_dexcv_font), __FILE__); ?>"  id="dex_bccf_captchaimg" alt="security code" border="0"  /><br />
+  <div class="fields" id="field-c2"><label><?php echo $l_sec_code_low; ?>:</label><div class="dfield"><input type="text" size="20" name="hdcaptcha_dex_bccf_post" id="hdcaptcha_dex_bccf_post" value="" /><div class="error message" id="hdcaptcha_error" generated="true" style="display:none;position: absolute; left: 0px; top: 25px;"></div><div class="clearer"></div></div></div> 
+<?php } ?>
+</div>
+<div id="cp_subbtn"><?php echo $l_continue; ?></div>
+</form>
